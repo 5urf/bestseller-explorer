3a4379dbbfceef97fb581bb745d4dd6b579a9cbd
LeeYunHak <sza0203@naver.com>
2025-04-11 17:08:58 +0900

    fix(Home): Home page CategoryCard component로 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
+import CategoryCard from "@/components/CategoryCard";
 import { fetchBestsellerLists } from "@/lib/api";
 import { Metadata } from "next";
-import Link from "next/link";
 import styles from "./page.module.css";
 
 export const metadata: Metadata = {
@@ -14,15 +14,11 @@ export default async function Home() {
     <main className={styles["container"]}>
       <section className={styles["gridContainer"]}>
         {results.map((result) => (
-          <Link
+          <CategoryCard
             key={result.list_name}
-            href={`/list/${result.list_name_encoded}`}
-            className={styles["categoryLink"]}
-          >
-            <article className={styles["categoryCard"]}>
-              <h2 className={styles["categoryName"]}>{result.display_name}</h2>
-            </article>
-          </Link>
+            displayName={result.display_name}
+            listNameEncoded={result.list_name_encoded}
+          />
         ))}
       </section>
     </main>
---
+++ src/components/CategoryCard.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import styles from "./CategoryCard.module.css";
+
+interface ICategoryCardProps {
+  displayName: string;
+  listNameEncoded: string;
+}
+
+export default function CategoryCard({
+  displayName,
+  listNameEncoded,
+}: ICategoryCardProps) {
+  return (
+    <Link href={`/list/${listNameEncoded}`} className={styles["categoryLink"]}>
+      <article className={styles["categoryCard"]}>
+        <h2 className={styles["categoryName"]}>{displayName}</h2>
+      </article>
+    </Link>
+  );
+}
