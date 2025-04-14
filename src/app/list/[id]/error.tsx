7bea7ca17d5dfc0c861d467d08aa655ff5730bbb
LeeYunHak <sza0203@naver.com>
2025-04-14 12:18:12 +0900

    refactor: 디렉토리 구조 개선 및 중복 파일 제거
    홈페이지 관련 파일을 (home) 그룹 폴더로 이동
    루트 레벨 error.tsx를 공통으로 사용하도록 변경
    list/[id] 경로의 중복 error.tsx 파일 제거

--- src/app/list/[id]/error.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import ErrorDisplay from "@/components/ErrorDisplay";
-
-export default function Error({ error, reset }: IErrorProps) {
-  return <ErrorDisplay error={error} reset={reset} />;
-}
